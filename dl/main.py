e3e175d98ff38601d232414c5ca1b9ae56b4f221
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-05-23 18:16:14 +0300

    Copy in the model

--- dl/main.py
+++ dl/main.py
@@ -1,28 +1,34 @@
 import os
+from utils import Lambda, SpatialLinearTransformer, Module
+
+from tqdm.auto import tqdm
 
 import torch
-import torchvision as tv
+from torch import nn
+from torch.nn import functional as F
 import cv2
-
-import utils
+import torchvision
 
 
 class DocumentDataset(torch.utils.data.Dataset):
-    def __init__(self, path):
+    def __init__(self, repeat=1):
         super().__init__()
-        self.imgs = self.load_images(path)
+        self.repeat = repeat
+        self.imgs = self.load_images('.data/x')
+        self.target = self.load_images('.data/y')[0]
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.imgs) * self.repeat
 
     def __getitem__(self, idx):
-        return self.imgs[idx]
+        idx = idx % len(self.imgs)
+        return {'x': self.imgs[idx], 'y': self.target}
 
     @staticmethod
     def load_images(path):
         file_names = [f for f in os.listdir(path) if f.endswith('jpg')]
         imgs = []
-        scale = 0.2
+        scale = 0.1
         W, H = 2138, 2997
         w, h = int(W * scale), int(H * scale)
 
@@ -39,19 +45,71 @@ class DocumentDataset(torch.utils.data.Dataset):
         return torch.tensor(imgs, dtype=torch.float32)
 
     @staticmethod
-    def get_dl(bs, shuffle):
-        dataset = DocumentDataset(path='.data')
+    def get_dl(bs, shuffle, repeat=1):
+        dataset = DocumentDataset(repeat=repeat)
         dl = torch.utils.data.DataLoader(
             dataset=dataset, batch_size=bs, shuffle=shuffle)
         return dl
 
     @staticmethod
-    def example():
+    def sanity_check():
         import matplotlib.pyplot as plt
 
-        dl = DocumentDataset.get_dl(bs=8, shuffle=False)
+        dl = DocumentDataset.get_dl(bs=16, shuffle=False, repeat=10)
         it = iter(dl)
         batch = next(it)
-        batch.spread[4, 2, -3:].imshow(figsize=(16, 16))
+        batch['x'].wrap.grid(nr=4).imshow(figsize=(8, 8))
+        # plt.close()
 
-        # plt.imshow(img)
+
+class GeometricTransformModel(Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = torchvision.models.resnet50(pretrained=True)
+        self.st = SpatialLinearTransformer(1000, num_channels=1)
+
+    def forward(self, x):
+        self.features = self.feature_extractor(x)
+        x = x.mean(dim=1, keepdim=True)
+        y_hat = self.st([self.features, x])
+        return y_hat
+
+    def criterion(self, y_hat, y):
+        y = y.mean(dim=1, keepdim=True)
+        return F.binary_cross_entropy(y_hat, y)
+
+    def optim_step(self, optim, batch):
+        x, y = batch['x'], batch['y']
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+
+        if loss.requires_grad:
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+        return {
+            'loss': loss.item(),
+            'features': self.features,
+            'y_hat': y_hat,
+        }
+
+
+def main():
+    dl = DocumentDataset.get_dl(bs=16, shuffle=False, repeat=10)
+    model = GeometricTransformModel()
+    optim = torch.optim.Adam(model.parameters(), lr=0.000001)
+    epochs = 8
+
+    epoch_bar = tqdm(range(epochs))
+    for e in epoch_bar:
+        batch_bar = tqdm(dl)
+        for batch in batch_bar:
+            optim_info = model.optim_step(optim, batch)
+            loss = optim_info['loss']
+
+            batch_bar.set_description(f'Loss: {loss:.5f}')
+
+
+if __name__ == '__main__':
+    main()
